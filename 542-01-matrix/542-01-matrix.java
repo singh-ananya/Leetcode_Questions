fafe06e2cd939a305967d1f0473f1de523f6c5df
Ananya Singh <104223501+singh-ananya@users.noreply.github.com>
2022-05-22 21:48:48 +0530

    Time: 24 ms (34.73%), Space: 47.2 MB (73.35%) - LeetHub

---
+++ 542-01-matrix/542-01-matrix.java
@@ -0,0 +1,39 @@
+class Solution {
+    //we will use BFS coz it ensures minimum distance
+    int[][] dirs={{1,0},{-1,0},{0,1},{0,-1}};
+    public int[][] updateMatrix(int[][] mat) {
+        Queue<int[]> q=new ArrayDeque<>();
+        for(int i=0;i<mat.length;i++){
+            for(int j=0;j<mat[0].length;j++){
+                if(mat[i][j]==0){
+                    q.add(new int[]{i,j}); // we will put all the zeros into the queue because if we put 1's then we can loose the starting poins
+                }
+            }
+        }
+        int [][]ans=new int[mat.length][mat[0].length];
+        for(int e[]:ans){
+            Arrays.fill(e,-1); // -1 for unvisisted
+        }
+        int level=0;
+        while(q.size()>0){
+            int size=q.size();
+            while(size-->0){
+                int temp[]=q.remove();
+                int x=temp[0];
+                int y=temp[1];
+                if(ans[x][y]!=-1)
+                    continue;
+                ans[x][y]=level;
+                for(int dir[]:dirs)
+                {
+                    int i=x+dir[0];
+                    int j=y+dir[1];
+                    if(i>=0 && j>=0 && i<mat.length && j<mat[0].length)
+                        q.add(new int[]{i,j});
+                }
+            }
+            level++;
+        }
+        return ans;
+    }
+}
