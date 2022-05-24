c058afe81dbdc9d4f58cb647a28663b07cb12a88
Ananya Singh <104223501+singh-ananya@users.noreply.github.com>
2022-05-24 19:24:17 +0530

    Time: 1 ms (84.69%), Space: 44.3 MB (51.62%) - LeetHub

---
+++ 419-battleships-in-a-board/419-battleships-in-a-board.java
@@ -0,0 +1,25 @@
+class Solution {
+    int dirs[][]={{1,0},{0,1}};
+    public int countBattleships(char[][] board) {
+       int count=0;
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
+                if(board[i][j]=='X'){
+                    dfs(board,i,j);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    public void dfs(char[][] board,int i,int j){
+        board[i][j]='.';
+        for(int dir[]:dirs){
+            int x=i+dir[0];
+            int y=j+dir[1];
+            if(x>=0 && y>=0 && x<board.length && y<board[0].length && board[x][y]!='.'){
+                dfs(board,x,y);
+            }
+        }
+    }
+}
