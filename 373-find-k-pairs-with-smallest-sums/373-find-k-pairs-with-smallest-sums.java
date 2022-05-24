1c3e038bf35fbc206040508750221771aa31b53d
Ananya Singh <104223501+singh-ananya@users.noreply.github.com>
2022-05-24 16:09:02 +0530

    Time: 29 ms (91.66%), Space: 57.6 MB (83.94%) - LeetHub

--- 373-find-k-pairs-with-smallest-sums/373-find-k-pairs-with-smallest-sums.java
+++ 373-find-k-pairs-with-smallest-sums/373-find-k-pairs-with-smallest-sums.java
@@ -18,7 +18,7 @@ class Solution {
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
         PriorityQueue<pair> pq=new PriorityQueue<>();
         List<List<Integer>> res=new ArrayList<>();
-        for(int i=0;i<nums1.length;i++){
+        for(int i=0;i<nums1.length;i++){//add first nums1 length pairs to the pq
             pq.add(new pair(nums1[i],nums2[0],nums1[i]+nums2[0],0));
         }
         while(k>0 && pq.size()>0){
@@ -28,7 +28,7 @@ class Solution {
             ans.add(curr.a2);
             res.add(ans);
             int index=curr.idx;
-            if(index<nums2.length-1){
+            if(index<nums2.length-1){ //if the element which is removed has more elements after itself(from nums2 array) than add //that element into the priority queue
                 pq.add(new pair(curr.a1,nums2[index+1],curr.a1+nums2[index+1],index+1));
             }
             k--;
