94f89995dcd49e98b05d55f38535db25e7ebba76
Ananya Singh <104223501+singh-ananya@users.noreply.github.com>
2022-06-23 23:57:15 +0530

    Time: 73 ms (26.11%), Space: 69.1 MB (61.85%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.java
+++ 630-course-schedule-iii/630-course-schedule-iii.java
@@ -1,21 +1,28 @@
 class Solution {
+// Sort courses by last day in ascending order.
+// Max Heap to track the max duration courses using Priority Queue.
+// Time + duration and put duration to max heap.
+// If pass the last day, remove the courses takes longest time.
+// Heap size is the max courses number we take.
+// Time complexity: O(n.logn)
+// Space complexity: O(n)
     public int scheduleCourse(int[][] courses) {
         Arrays.sort(courses,(int[] a,int[] b)->{  //lambda function to sort array on the basis of last day
             return a[1]-b[1];
         });
-        PriorityQueue<Integer> heap=new PriorityQueue<>((a,b)->{
-            return b-a;
+        PriorityQueue<Integer> pq=new PriorityQueue<>((a,b)->{
+            return b-a;//sort in desc order, max heap
         });
         int time = 0;
-        for(int[] course: courses) {
-            int duration = course[0], lastDay = course[1];
-            time += duration;
-            heap.add(duration);
-            // if pass the last day, remove courses these take longest time.
-            if(time > lastDay) {
-                time -= heap.poll();
+        for(int c[]:courses){
+            int duration=c[0];
+            int lastday=c[1];
+            pq.add(duration);
+            time+=duration;
+            if(time>lastday){
+                time-=pq.remove();  
             }
         }
-        return heap.size();
+        return pq.size();
     }
 }
