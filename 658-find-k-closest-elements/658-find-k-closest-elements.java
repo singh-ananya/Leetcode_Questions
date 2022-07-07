45c6f1e6cb1e164d060fd47b26a6c9f83180803d
Ananya Singh <104223501+singh-ananya@users.noreply.github.com>
2022-07-08 01:08:16 +0530

    Time: 95 ms (5.87%), Space: 69.4 MB (5.49%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.java
+++ 658-find-k-closest-elements/658-find-k-closest-elements.java
@@ -1,23 +1,20 @@
 class Solution {
-    public class Pair{
+    public class Pair implements Comparable<Pair>{
         int key;
         int num;
         Pair(int key,int num){
             this.key = key;
             this.num = num;
         }
+        public int compareTo(Pair o){
+            if(o.key==this.key){
+                return o.num-this.num;
+            }
+            return o.key-this.key;
+        }
     }
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        PriorityQueue<Pair> MaxHeap = new PriorityQueue<>(new Comparator<Pair>(){
-           public int compare(Pair p1,Pair p2){
-               if(p1.key == p2.key){
-                   return p2.num - p1.num;
-               }
-               else{
-                   return p2.key - p1.key;
-               }
-           } 
-        });
+        PriorityQueue<Pair> MaxHeap = new PriorityQueue<>();
         List<Integer> ans = new ArrayList<>();
         for(int i = 0;i<arr.length;i++){
             MaxHeap.add(new Pair(Math.abs(arr[i]-x),arr[i]));
