880cddbf4d1300944bed21c73e3a538aac8cd27b
Ananya Singh <104223501+singh-ananya@users.noreply.github.com>
2022-07-13 21:45:15 +0530

    Time: 239 ms (62.76%), Space: 131.2 MB (39.69%) - LeetHub

--- 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.java
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.java
@@ -1,4 +1,6 @@
 class Solution {
+        // Time complexity: O(mn)
+        // Space complexity: O(mn)
     public int minimumObstacles(int[][] grid) {
         int m=grid.length;
         int n=grid[0].length;
