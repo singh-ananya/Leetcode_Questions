e29966864e4150f9a9fa4801f337b6c5f411981a
Ananya Singh <104223501+singh-ananya@users.noreply.github.com>
2022-09-07 09:33:05 -0700

    Time: 38 ms (22.20%), Space: 63.6 MB (16.22%) - LeetHub

---
+++ 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+   public String tree2str(TreeNode root) {
+        if(root == null) return "";
+        StringBuilder sb = new StringBuilder();
+        
+        int val = root.val;
+        
+        // Get strig from both subtrees
+        String left = tree2str(root.left);
+        String right = tree2str(root.right);
+        
+        sb.append(val+"");
+        
+        // Ignore left part only if both left and right children are null
+        // Else append left, even if empty parenthesis
+        sb.append((left.isEmpty() && right.isEmpty()) ? "" : "("+left+")");
+        
+        // Append right part only if right child is not null
+        sb.append((right.isEmpty()) ? "" : "("+right+")");
+        
+        return sb.toString();
+    }
+    
+}
