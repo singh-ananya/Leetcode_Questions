fd5df7cd0439c4a13f15e2df9bdec31f550cfbc4
Ananya Singh <104223501+singh-ananya@users.noreply.github.com>
2022-09-10 10:04:52 -0700

    Time: 5 ms (55.67%), Space: 43.5 MB (29.59%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.java
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,53 +1,53 @@
-class Solution {
-    public int maxProfit(int k, int[] prices) {
-        
-        int n = prices.length;
-        int cap = k; // no of transactions allowed
-        int buy = 1; // can buy or not
-        Integer[][][] dp = new Integer[n][2][k+1];
-        return maxProfit(0, buy, cap, prices, n, dp);
-    }
-    
-    private int maxProfit(int i, int buy, int cap, int[] prices, int n, Integer[][][] dp){
-        
-        if(i == n || cap == 0) return 0;
-        
-        if(dp[i][buy][cap] != null) return dp[i][buy][cap];
-        // if can buy new stack else sell
-        if(buy == 1){
-            return dp[i][buy][cap] = Math.max(-prices[i] + maxProfit(i+1, 0, cap, prices, n, dp)
-                             , 0 + maxProfit(i+1, 1, cap, prices, n, dp));                     
-        }
-        else{
-            return dp[i][buy][cap] = Math.max(prices[i] + maxProfit(i+1, 1, cap-1, prices, n, dp)
-                             , 0 + maxProfit(i+1, 0, cap, prices, n, dp));
-        }
-    }
-}
-
-
-
-// // Tabulation Solution
-// // TC - O(n*2*k) , SC - O(n*2*k)
 // class Solution {
 //     public int maxProfit(int k, int[] prices) {
         
 //         int n = prices.length;
-//         int[][][] dp = new int[n+1][2][k+1];
-//         for(int i = n-1; i >= 0; i--){
-//             for(int buy = 0; buy < 2; buy++){
-//                 for(int cap = 1; cap < k+1; cap++){
-//                     if(buy == 1){
-//                         dp[i][buy][cap] = Math.max(-prices[i] + dp[i+1][0][cap]
-//                                                             , 0 + dp[i+1][1][cap]);                     
-//                         }
-//                     else{
-//                         dp[i][buy][cap] = Math.max(prices[i] + dp[i+1][1][cap-1]
-//                                                             , 0 + dp[i+1][0][cap]);
-//                     }
-//                 }
-//             }
+//         int cap = k; // no of transactions allowed
+//         int buy = 1; // can buy or not
+//         Integer[][][] dp = new Integer[n][2][k+1];
+//         return maxProfit(0, buy, cap, prices, n, dp);
+//     }
+    
+//     private int maxProfit(int i, int buy, int cap, int[] prices, int n, Integer[][][] dp){
+        
+//         if(i == n || cap == 0) return 0;
+        
+//         if(dp[i][buy][cap] != null) return dp[i][buy][cap];
+//         // if can buy new stack else sell
+//         if(buy == 1){
+//             return dp[i][buy][cap] = Math.max(-prices[i] + maxProfit(i+1, 0, cap, prices, n, dp)
+//                              , 0 + maxProfit(i+1, 1, cap, prices, n, dp));                     
 //         }
-//         return dp[0][1][k];
+//         else{
+//             return dp[i][buy][cap] = Math.max(prices[i] + maxProfit(i+1, 1, cap-1, prices, n, dp)
+//                              , 0 + maxProfit(i+1, 0, cap, prices, n, dp));
+//         }
 //     }
 // }
+
+
+
+// Tabulation Solution
+// TC - O(n*2*k) , SC - O(n*2*k)
+class Solution {
+    public int maxProfit(int k, int[] prices) {
+        
+        int n = prices.length;
+        int[][][] dp = new int[n+1][2][k+1];
+        for(int i = n-1; i >= 0; i--){
+            for(int buy = 0; buy < 2; buy++){
+                for(int cap = 1; cap < k+1; cap++){
+                    if(buy == 1){
+                        dp[i][buy][cap] = Math.max(-prices[i] + dp[i+1][0][cap]
+                                                            , 0 + dp[i+1][1][cap]);                     
+                        }
+                    else{
+                        dp[i][buy][cap] = Math.max(prices[i] + dp[i+1][1][cap-1]
+                                                            , 0 + dp[i+1][0][cap]);
+                    }
+                }
+            }
+        }
+        return dp[0][1][k];
+    }
+}
