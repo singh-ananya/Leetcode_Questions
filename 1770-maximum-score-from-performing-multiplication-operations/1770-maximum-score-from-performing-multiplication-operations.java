991a65b1f813156c8f25793b605d687e9bc8bc3a
Ananya Singh <104223501+singh-ananya@users.noreply.github.com>
2022-09-16 11:31:20 -0700

    Time: 208 ms (55.38%), Space: 100.1 MB (77.24%) - LeetHub

---
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maximumScore(int[] nums, int[] multipliers) {
+        int[][] dp=new int[multipliers.length][multipliers.length];
+        for(int[] a:dp){
+            Arrays.fill(a,-1001);
+        }
+        int diff=nums.length-multipliers.length;
+        return solve(0,0,nums.length-1,nums,multipliers,dp,diff);
+    }
+    int solve(int x,int i,int j,int[] nums,int[] mult,int[][] dp,int diff){
+        if(x>=mult.length || i>j) return 0;
+        if(dp[i][j-diff]!=-1001) return dp[i][j-diff];
+        
+        int start=mult[x]*nums[i]+solve(x+1,i+1,j,nums,mult,dp,diff);
+        int end=mult[x]*nums[j]+solve(x+1,i,j-1,nums,mult,dp,diff);
+        return dp[i][j-diff]=Math.max(start,end);
+    }
+}
